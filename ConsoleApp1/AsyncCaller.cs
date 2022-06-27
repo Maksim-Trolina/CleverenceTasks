c43c8f8771c5a88d828838e5fef88572d23b16b6
Maksim.Panasenko <maks.panasenko01@bk.ru>
2022-06-28 01:08:15 +0300

    Initial

---
+++ ConsoleApp1/AsyncCaller.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class AsyncCaller
+    {
+        readonly EventHandler handler;
+
+        public AsyncCaller(EventHandler handler)
+        {
+            this.handler = handler ?? throw new ArgumentNullException(nameof(handler), "Null value is invalid");
+        }
+
+        public bool Invoke(int timeout, object sender, EventArgs e)
+        {
+            var task = Task.Run(() => handler.Invoke(sender, e));
+
+            return task.Wait(timeout);
+        }
+    }
+}
---
+++ ConsoleApp1/Server.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+
+namespace ConsoleApp1
+{
+    static internal class Server
+    {
+        static int count;
+        static readonly ReaderWriterLockSlim locker = new();
+
+        public static void AddToCount(int value)
+        {
+            locker.EnterWriteLock();
+            try
+            {
+                checked
+                {
+                    count += value;
+                }
+            }
+            finally
+            {
+                locker.ExitWriteLock();
+            }
+        }
+        public static int GetCount()
+        {
+            locker.EnterReadLock();
+            try
+            {
+                return count;
+            }
+            finally
+            {
+                locker.ExitReadLock();
+            }
+        }
+    }
+}
